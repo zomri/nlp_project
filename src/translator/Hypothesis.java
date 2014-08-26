93f2b53cd6e7b984774c159021f4074283f31fdf
oshai <ohadshai@gmail.com>
2014-08-26 22:38:52 +0300

    hypothesis

--- src/translator/Hypothesis.java
+++ src/translator/Hypothesis.java
@@ -1,5 +1,14 @@
 package translator;
 
+import java.util.List;
+
+import com.beust.jcommander.internal.Lists;
+
 public class Hypothesis {
 
+	List<Hypothesis> prev = Lists.newArrayList();
+	List<Hypothesis> next = Lists.newArrayList();
+	List<String> words = Lists.newArrayList();
+	List<Boolean> coverage = Lists.newArrayList();
+	double score;
 }
