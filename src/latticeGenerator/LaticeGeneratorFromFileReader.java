78533e8d7e2f56b3b31249456ac13d4068e180b0
oshai <ohadshai@gmail.com>
2014-09-10 16:55:25 +0300

    latice generator read/write

---
+++ src/latticeGenerator/LaticeGeneratorFromFileReader.java
@@ -0,0 +1,54 @@
+package latticeGenerator;
+
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
+import com.google.common.base.Splitter;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multiset;
+import common.TextFileUtils;
+
+import edu.stanford.nlp.util.Pair;
+
+public class LaticeGeneratorFromFileReader {
+
+	private Pair<Integer, Integer> span;
+	private String latticeFilename;
+
+	public LaticeGeneratorFromFileReader(String latticeFilename) {
+		super();
+		this.latticeFilename = latticeFilename;
+	}
+
+	public Map<Pair<Integer, Integer>, Multiset<Pair<String, Double>>> readLattice() {
+		Map<Pair<Integer, Integer>, Multiset<Pair<String, Double>>> $ = Maps.newHashMap();
+		Predicate<String> linePredicate = new Predicate<String>() {
+			@Override
+			public boolean apply(String line) {
+				if (line.endsWith(":")) {
+					String patternString = "(\\d)-(\\d):";
+					Pattern pattern = Pattern.compile(patternString);
+					Matcher matcher = pattern.matcher(line);
+					matcher.find();
+					span = new Pair<Integer, Integer>(Integer.valueOf(matcher.group(1)), Integer.valueOf(matcher
+							.group(2)));
+					$.put(span, HashMultiset.create());
+				} else {
+					List<String> lineSplitted = Splitter.on("\t").splitToList(line);
+					double prob = Double.parseDouble(lineSplitted.get(lineSplitted.size() - 1));
+					List<String> theSentence = lineSplitted.subList(0, lineSplitted.size() - 1);
+					$.get(span).add(new Pair<String, Double>(Joiner.on(" ").join(theSentence), prob));
+				}
+				return true;
+			}
+		};
+		TextFileUtils.getContentByLines(latticeFilename, linePredicate);
+		return $;
+
+	}
+}
---
+++ src/latticeGenerator/LatticeGeneratorFileWriter.java
@@ -0,0 +1,126 @@
+package latticeGenerator;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.beust.jcommander.internal.Sets;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multiset;
+
+import edu.stanford.nlp.util.Pair;
+import phrase_table.PhraseTableReaderWriter;
+
+public class LatticeGeneratorFileWriter {
+
+	private String inputSentence;
+	private String latticeFilename;
+	private Map<String, Multiset<Pair<String, Double>>> phraseTableMap;
+	
+	public static void main(String[] args) {
+		
+		String input = "+W LK $WB L+ DRK +K M+ DBRH DM$Q W+ B+ HAT W+ M$XT AT XZAL L+ HMLK EL ARM";
+		
+		PhraseTableReaderWriter ptrw = new PhraseTableReaderWriter("d:\\phrase_table.txt");
+		Map<String, Multiset<Pair<String, Double>>> map = ptrw.read();
+
+		LatticeGeneratorFileWriter lg = new LatticeGeneratorFileWriter(input, map, "d:\\lattice.txt");
+
+		lg.createLaticeFile();
+	}
+
+	public LatticeGeneratorFileWriter(String inputSentence, Map<String, Multiset<Pair<String, Double>>> map, String latticeFilename)
+	{
+		this.setInputSentence(inputSentence);
+		this.setLatticeFilename(latticeFilename);
+		this.setPhraseTableFilename(map);
+	}
+
+	private void setPhraseTableFilename(
+			Map<String, Multiset<Pair<String, Double>>> map) {
+		this.phraseTableMap = map;
+		
+	}
+
+	
+	
+	public Map<Pair<Integer, Integer>, Multiset<Pair<String, Double>>> createLaticeFile()
+	{
+
+		Map<Pair<Integer, Integer>, Multiset<Pair<String, Double>>> map = Maps.newHashMap();
+		
+		Charset charset = Charset.forName("UTF-8");	
+		Path file = Paths.get(latticeFilename);
+		BufferedWriter writer = null;
+		try {
+			writer = Files.newBufferedWriter(file,charset);
+
+			//Foreach span of the sentence - find the relevant translations - and their scores (not in particular order)
+			String[] words = inputSentence.split("\\s");
+
+			for (int i=0; i<words.length; ++i)
+			{
+				String phrase = words[i];
+				for (int j = i; j<words.length; ++j)
+				{
+					if(j!=i)
+					{
+						phrase += " "+ words[j];
+					}
+
+
+					Multiset<Pair<String, Double>> set = phraseTableMap.get(phrase);
+					if (set != null)
+					{
+						writer.write(new Integer(i+1).toString()+"-"+new Integer(j+1).toString()+":\n");
+
+						Pair<Integer, Integer> rangePair = new Pair<Integer, Integer>(i+1, j+1);
+						Multiset<Pair<String, Double>> innerSet = HashMultiset.create();
+			    		map.put(rangePair, innerSet);
+						
+						for (Pair<String, Double> pair : set)
+						{
+							writer.write(pair.first+"\t"+pair.second+"\n");
+							innerSet.add(new Pair<String,Double>(pair.first,pair.second));
+						}
+					}
+					else
+					{
+						System.out.println("Failed to find translation for '"+phrase+"'");
+					}
+				}
+			}
+			writer.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		return map;
+	}
+
+	public String getInputSentence() {
+		return inputSentence;
+	}
+
+	public void setInputSentence(String inputSentence) {
+		this.inputSentence = inputSentence;
+	}
+
+	public String getLatticeFilename() {
+		return latticeFilename;
+	}
+
+	public void setLatticeFilename(String latticeFilename) {
+		this.latticeFilename = latticeFilename;
+	}
+
+}
